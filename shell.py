6b4fd158bb41957868ac6daa710ef22795895697
dengzhaoxuan <dengzhaoxuan@dangdang.com>
2018-12-13 18:55:45 +0800

    初步完成删除git分支脚本

--- shell.py
+++ shell.py
@@ -1,13 +1,26 @@
 import os
+
 print("输入排除的分支名：（【空格隔开】默认排除mastar和remote）")
 exceptBranch = input()
 exceptBranchs = exceptBranch.split(" ")
 exceptBranchs.append("master")
 branchSource = os.popen("git branch -a").read()
 branchs = branchSource.split("\n")
-for branch in branchs:
-    if "remotes" not in branch :
+
+
+# 判断是否需要排除这个分支
+def showExcept(branchTemp):
+    if "remotes" in branchTemp:
+        return True
+    else:
         for exceptItem in exceptBranchs:
-            if branch not in exceptItem:
-                print("git branch -d "+branch)
-                result = os.popen("git branch -d"+branch)
+            if exceptItem in branchTemp:
+                return True
+    return False
+
+    pass
+
+
+for branch in branchs:
+    if not showExcept(branch):
+        os.popen("git branch -d" + branch)
