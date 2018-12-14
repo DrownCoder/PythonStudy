11c5a317fab07d40110f0739b2ae898c14c4870f
dengzhaoxuan <dengzhaoxuan@dangdang.com>
2018-12-14 13:10:09 +0800

    多目录结果清理

--- shell.py
+++ shell.py
@@ -1,13 +1,37 @@
 import os
+
 print("输入排除的分支名：（【空格隔开】默认排除mastar和remote）")
 exceptBranch = input()
 exceptBranchs = exceptBranch.split(" ")
 exceptBranchs.append("master")
-branchSource = os.popen("git branch -a").read()
-branchs = branchSource.split("\n")
-for branch in branchs:
-    if "remotes" not in branch :
+
+
+# 判断是否需要排除这个分支
+def showExcept(branchTemp):
+    if "remotes" in branchTemp:
+        return True
+    else:
         for exceptItem in exceptBranchs:
-            if branch not in exceptItem:
-                print("git branch -d "+branch)
-                result = os.popen("git branch -d"+branch)
+            if exceptItem in branchTemp:
+                return True
+    return False
+    pass
+
+
+# 删除分支
+def deleteBranch():
+    branchSource = os.popen("git branch -a").read()
+    branchs = branchSource.split("\n")
+    for branch in branchs:
+        if not showExcept(branch):
+            os.popen("git branch -d" + branch)
+
+
+path = os.popen("ls").read()
+projectPaths = path.split("\n")
+for project in projectPaths:
+    print("清理" + project + "目录下的分支...")
+    os.popen("cd " + project)
+    deleteBranch()
+    print("清理完成")
+    os.popen("cd..")
